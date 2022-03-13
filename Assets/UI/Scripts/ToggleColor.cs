add9c9a48652ba16462e120ed2f2a17c98091324
Fritzpatrick Leroy <fritzpatrickleroy@gmail.com>
2022-03-12 19:14:53 -0500

    minor push - test
    Reinstalled unity, making sure everything works.

--- Assets/UI/Scripts/ToggleColor.cs
+++ Assets/UI/Scripts/ToggleColor.cs
@@ -7,12 +7,25 @@ public class ToggleColor : MonoBehaviour
 {
 
     bool colorOn = false;
-    
+    public Color rgbs = new Color(0.353f, 0.353f, 0.353f, 1f);
+
     public void OnButtonPress() {
 
         colorOn = !colorOn;
         if (colorOn) {
-            this.gameObject.GetComponent<Image>().color = new Color(0.353f, 0.353f, 0.353f, 1f);
+            this.gameObject.GetComponent<Image>().color = rgbs;
+        } else {
+            this.gameObject.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+        }
+
+    }
+
+
+    public void ForceOn() {
+
+        colorOn = true;
+        if (colorOn) {
+            this.gameObject.GetComponent<Image>().color = rgbs;
         } else {
             this.gameObject.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
         }
@@ -23,7 +36,7 @@ public class ToggleColor : MonoBehaviour
 
         colorOn = false;
         if (colorOn) {
-            this.gameObject.GetComponent<Image>().color = new Color(0.353f, 0.353f, 0.353f, 1f);
+            this.gameObject.GetComponent<Image>().color = rgbs;
         } else {
             this.gameObject.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
         }
